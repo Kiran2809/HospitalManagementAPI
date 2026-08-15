1792aafe62da30c1414f89f857c41d31baceb250
Kiran Kumar <kirankumarkothapatnam@gmail.com>
2026-08-15 16:21:43 +0530

    Enable Swagger for IIS

--- HospitalManagementAPI/Program.cs
+++ HospitalManagementAPI/Program.cs
@@ -110,11 +110,10 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
